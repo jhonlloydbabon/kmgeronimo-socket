23f907a87851adeb97403719562b41ee3c948dd8
jhonlloydBabon <jlbabon12@gmail.com>
2023-12-03 08:11:18 -0800

    change web route v2

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000" || "exp://192.168.254.100:19000" || "https://updated-km-frontend.vercel.app", 
+    origin: "http://localhost:3000/" || "exp://192.168.254.100:19000" || "https://updated-km-frontend.vercel.app", 
     methods: ["GET", "POST"],
   },
 });
